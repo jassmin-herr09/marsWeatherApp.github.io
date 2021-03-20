684dc7249ad736ae2859dccea0cb7e09632baa84
jassmin-herr <jherr0905@gmail.com>
2021-03-20 13:59:15 -0500

    first commit to set up

---
+++ README.md
@@ -0,0 +1,22 @@
+# Mars Weather App
+
+Insight's weather mesasurements from the surface of Mars.
+
+## Technologies Used
+- HTML5
+- CSS3
+- Javascript
+- Jquery
+
+
+## Screenshots
+
+![screenshot1]()
+
+## Getting Started
+
+[Click here](#) to see the app in your browser
+
+## Future Enhancements
+
+- TBD
---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+// constant variables - data that never changes
+ const BASE_URL = 'https://api.nasa.gov/insight_weather/?api_key=bGStYxJHMJPGTuwbsTUe2Ltjq76wUlkm8DY2f54E&feedtype=json&ver=1.0';
+
+// state variables - data that changes
+
+//let weather;
+
+// cached element references - parts of the dom we need to touch
+
+//const $weather = $('#weather');
+
+// event listeners - capture and respond to events i.e. user clicks on something
+// functions - code that represents actions taken/carried out.
+
+init ();
+
+function init() {
+    getData();
+}
+
+
+function getData() {
+    $.ajax(BASE_URL)
+        .then(function (data) {
+            
+            console.log(data);
+           // weather = data;
+            render();
+        }, function (error) {
+            console.log(error);
+        });
+}
+
+function render () {
+   // console.log(weather);
+}
+
+
