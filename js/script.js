9c359fac16b59f753870715f1bdcdeb9dcd18c54
jassmin-herr <jherr0905@gmail.com>
2021-03-23 20:16:40 -0500

    more updates to html css

--- js/script.js
+++ js/script.js
@@ -2,17 +2,13 @@
 // constant variables - data that never changes
  const BASE_URL = 'https://api.nasa.gov/insight_weather/?api_key=bGStYxJHMJPGTuwbsTUe2Ltjq76wUlkm8DY2f54E&feedtype=json&ver=1.0';
 
-// state variables - data that changes
 
-let weather = [];
+let weather;
+let values = [];
 
-// cached element references - parts of the dom we need to touch
 
 const $weather = $('#weather');
 
-
-// event listeners - capture and respond to events i.e. user clicks on something
-// functions - code that represents actions taken/carried out.
 
 init ();
 
@@ -34,7 +30,25 @@ function getData() {
 }
 
 function render () {
-   // console.log(weather);
+    Object.values(weather).forEach(value => {
+       // console.log(value);
+        values.push(value);
+    });
+    values.pop()
+    values.pop()
+    values.forEach(item => {
+       // console.log(item);
+        let newdate = item.First_UTC;
+       // console.log(newdate.slice(0,10)); 
+        item.First_UTC = newdate.slice(0,10);
+        
+        const $li = $(`<li><span>DATE : </span> ${item.First_UTC.slice(0,10)} <span>MAX : </span>${item.PRE.mx} <span>AV : </span>${item.PRE.av} <span>MIN : </span>${item.PRE.mn}</li>`)
+        $(`ul`).append($li)
+    });
+
+
+   
+
 }
 
 
